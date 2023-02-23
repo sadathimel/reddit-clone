2123a066f81fd9fe62a250538c2a3ffcbcdc0fd4
sadathimel <sadat.himel@gmail.com>
2023-02-23 16:51:24 +0600

    updated

--- api/routes/api.php
+++ api/routes/api.php
@@ -18,6 +18,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('text', function(){
-
+Route::group(['prefix' => 'access'], function () {
+    Route::post('register', [\App\Http\Controllers\AuthController::class, 'register']);
+    Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
+    Route::post('logout', [\App\Http\Controllers\AuthController::class, 'logout']);
 });
+
